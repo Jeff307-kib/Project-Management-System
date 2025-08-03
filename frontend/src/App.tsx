324eb8cf81fc5428ea7d4b11120a0a53fb1c8830
Jeff307-kib <wunnatunsai940@gmail.com>
2025-08-03 22:54:27 +0630

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
